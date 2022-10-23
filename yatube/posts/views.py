bfca7f1c747c09537295f7bc2d429ab27e1571b1
Ольга Жолудева <joludeva.olga@yandex.ru>
2022-10-23 10:56:35 +0300

    Выполнены задания 6-го спринта

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import PostForm, CommentForm
 from posts.utils import paginator_func
+from .models import Group, Post, User, Follow
 
 
 def index(request):
